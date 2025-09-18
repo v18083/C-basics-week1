37319f87876a10ee9d7df170d652b178b803e30a
v18083 <musondav137@gmail.com>
2025-09-17 23:52:22 -0700

    my first project

---
+++ c-basics-week1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#ask user to enter name
+  echo "vincent"
+#ask user to enter age
+  echo "22"
+#ask user to enter hobby
+  echo "football"
---
+++ main.c
@@ -0,0 +1,7 @@
+#!/bin/bash
+name=vincent
+if [ "$name" -me vincent ];then
+  echo "age is 22"
+else
+  echo "hobby is sportball"
+fi
