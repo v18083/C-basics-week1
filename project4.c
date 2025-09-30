9dc2d5e984cc3867d6141a22823d993a39b35e99
U-VINCENT\muson <musondav137@gmail.com>
2025-09-30 13:19:27 -0700

    temperature converter

--- project4.c
+++ project4.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 int main(){
-    double num1, num2;
-    printf("enter first number:");
-    scanf("%lf", &num1);
-    printf("enter second number");
-    scanf("&lf", &num2);
-    printf("/nsum =  &.2lf/n", num1 + num2);
-    printf("difference = &.2lf/n", num1 - num2);
-    printf("product = &.lf/n", num1 * num2);
-if (num2 != 0){
-    printf("quotient = &.2lf/n", num1 / num2);
-} else {
-   printf("quotient = undefined (division by zero)/n");
-}
+    int celsius;
+    int fahrenheit;
+    printf("enter temperature in celsius:");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9/5) + 32;
+    printf("in fahrenheit: %.1f/n", fahrenheit);
+    
+    printf("enter temperature in fahrenheit:");
+    scanf("%f", &fahrenheit);
+    celsius = (fahrenheit - 32) * 5/9;
+    printf("in celsius: %.1f/n", celsius);
 return 0;
 }
